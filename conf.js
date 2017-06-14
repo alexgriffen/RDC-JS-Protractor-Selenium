d9000ab357b05cd48b6b3ae0bcb5ea97aedfaed7
ndmanvar <ndmanvar@yahoo.com>
2017-06-13 19:33:04 -0700

    adding in conf.js

---
+++ conf.js
@@ -0,0 +1,34 @@
+exports.config = {
+
+    seleniumAddress: 'http://us1.appium.testobject.com/wd/hub',
+    specs: ['specs/*spec.js'],
+
+    // restartBrowserBetweenTests: true,
+
+    onPrepare: function() {
+        var caps = browser.getCapabilities();
+    },
+
+    multiCapabilities: [{
+        testobject_api_key: process.env.TESTOBJECT_API_KEY,
+        testobject_device: 'Google_Pixel_real',
+        testobject_test_name: "android - test name",
+        testobject_cache_device: "true",
+        browserName: 'chrome',
+        shardTestFiles: false,
+        maxInstances: 25
+    }
+    // add more configurations here
+    ],
+
+    onComplete: function(result) {
+        console.log("result is " + result);
+
+        var printSessionId = function(jobName) {
+            browser.getSession().then(function(session) {
+                console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+            });
+        }
+        printSessionId("Insert Job Name Here");
+    }
+};
