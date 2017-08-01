1ec0f7bdc737925da492d3114a1df61f0b407d2f
Lasse Häll <lasse@saucelabs.com>
2017-08-01 19:55:13 +0200

    Add test result upload and parallelized device sets

--- conf.js
+++ conf.js
@@ -1,3 +1,5 @@
+var TestObject = require('./node_modules/testobject_api/lib/TestObject');
+
 exports.config = {
 
     seleniumAddress: 'http://eu1.appium.testobject.com/wd/hub',
@@ -12,21 +14,41 @@ exports.config = {
     multiCapabilities: [{
         testobject_api_key: process.env.TESTOBJECT_API_KEY,
         testobject_device: 'Google_Pixel_real',
-        testobject_test_name: "android - test name",
+        testobject_test_name: Date.now() + " - Static",
         testobject_cache_device: "true",
+        testobject_suite_name: "Static device allocation",
         browserName: 'chrome',
-        shardTestFiles: false,
-        maxInstances: 25
-    }
+        shardTestFiles: true,
+        maxInstances: 3,
+    }, {
+        testobject_api_key: process.env.TESTOBJECT_API_KEY,
+        testobject_test_name: Date.now() + " - Dynamic",
+        testobject_cache_device: "false",
+        testobject_suite_name: "Dynamic device allocation",
+        platformName: "Android",
+        platformVersion: "7",
+        phoneOnly: "true",
+        browserName: 'chrome',
+        shardTestFiles: true,
+        maxInstances: 3
+    },
     // add more configurations here
     ],
 
     onComplete: function(result) {
+        var myAccount = new TestObject({
+            username: process.env.TESTOBJECT_USER_NAME,
+            apiKey: process.env.TESTOBJECT_API_KEY,
+            password: process.env.TESTOBJECT_PASSWORD
+        });
+
         console.log("result is " + result);
 
         var printSessionId = function(jobName) {
             browser.getSession().then(function(session) {
                 console.log('SauceOnDemandSessionID=' + session.getId() + ' job-name=' + jobName);
+                myAccount.updateTest(session.getId(), {"passed": result}, function(res){
+                });
             });
         }
         printSessionId("Insert Job Name Here");
